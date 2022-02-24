036cfe400f4f8035aae4fa0ebeda1af0d8191eae
Cesar Sanchez <cesanchezesc@gmail.com>
2022-02-24 09:03:59 -0600

    First completed draft, ready to start testing

--- public/js/post.js
+++ public/js/post.js
@@ -1,48 +1,27 @@
 const newFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const name = document.querySelector('#project-name').value.trim();
-    const needed_funding = document.querySelector('#project-funding').value.trim();
-    const description = document.querySelector('#project-desc').value.trim();
-  
-    if (name && needed_funding && description) {
-      const response = await fetch(`/api/projects`, {
-        method: 'POST',
-        body: JSON.stringify({ name, needed_funding, description }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to create project');
-      }
+  event.preventDefault();
+
+  const content = document.querySelector('#post-content').value.trim();
+  const date = new Date();
+  const post_id = document.querySelector("#post").getAttribute('data-id')
+
+
+  if (content && date && post_id) {
+    const response = await fetch(`/api/comments`, {
+      method: 'POST',
+      body: JSON.stringify({content, date_created, post_id}),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace(`posts/${post_id}`);
+    } else {
+      alert('Failed to create comment');
     }
-  };
-  
-  const delButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
-  
-      const response = await fetch(`/api/projects/${id}`, {
-        method: 'DELETE',
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to delete project');
-      }
-    }
-  };
-  
-  document
-    .querySelector('.new-project-form')
-    .addEventListener('submit', newFormHandler);
-  
-  document
-    .querySelector('.project-list')
-    .addEventListener('click', delButtonHandler);
-  
+  }
+};
+
+
+document.querySelector('#comment-form').addEventListener('submit', newFormHandler);
